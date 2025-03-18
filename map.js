98aae9dcb61f482487a3b6bb669e0f47eb49ff27
JesterByte <ejjose94@gmail.com>
2025-03-18 19:46:05 +0800

    Update map zoom level and improve marker labels

--- map.js
+++ map.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     const kioskLat = parseFloat(urlParams.get('kiosk_lat'));
     const kioskLng = parseFloat(urlParams.get('kiosk_lng'));
   
-    const map = L.map("map").setView([kioskLat, kioskLng], 13); // Use kiosk location as the center
+    const map = L.map("map").setView([kioskLat, kioskLng], 19); // Use kiosk location as the center
     
     // Add OpenStreetMap tile layer
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -13,8 +13,8 @@ $(document).ready(function () {
     }).addTo(map);
   
     // Add markers for Kiosk and Grave location
-    L.marker([kioskLat, kioskLng]).addTo(map).bindPopup("Kiosk Location").openPopup();
-    L.marker([graveLat, graveLng]).addTo(map).bindPopup("Grave Location").openPopup();
+    L.marker([kioskLat, kioskLng]).addTo(map).bindPopup("Kiosk").openPopup();
+    L.marker([graveLat, graveLng]).addTo(map).bindPopup("Resting Place").openPopup();
   
     // Add routing control for directions between Kiosk and Grave
     L.Routing.control({
