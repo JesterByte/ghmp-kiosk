353a92388c8cae3f39baef98101976dd4a2a9f39
JesterByte <ejjose94@gmail.com>
2025-03-31 15:08:32 +0800

    Refactor database connection settings and remove unused map and carousel files

--- db_connection.php
+++ db_connection.php
@@ -1,13 +1,13 @@
 <?php
-$hostName = "localhost";
-$serverUsername = "u714551035_ghmp";
-$serverPassword = "P~t5GTVnuaZ";
-$databaseName = "u714551035_ghmp_db";
-
 // $hostName = "localhost";
-// $serverUsername = "root";
-// $serverPassword = "";
-// $databaseName = "ghmp_db";
+// $serverUsername = "u714551035_ghmp";
+// $serverPassword = "P~t5GTVnuaZ";
+// $databaseName = "u714551035_ghmp_db";
+
+$hostName = "localhost";
+$serverUsername = "root";
+$serverPassword = "";
+$databaseName = "ghmp_db";
 
 $conn = mysqli_connect($hostName, $serverUsername, $serverPassword, $databaseName);
 
